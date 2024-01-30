cfecce8c224b6ccfb2badec5282bf7ecca507c26
BD200166 <156713863+BD200166@users.noreply.github.com>
2024-01-30 20:44:34 +0100

    Update ArithmeticOperators.java

--- src/ArithmeticOperators.java
+++ src/ArithmeticOperators.java
@@ -5,13 +5,13 @@ public class ArithmeticOperators {
 		
 		int number1 = 1 + 2;  
 		System.out.println(number1);
-													//Arithmetic Operators
+								//Arithmetic Operators
 		int number2 = 1;
-		int number3 = 2;							//[+] Addition -> number = 1 + 2;
+		int number3 = 2;				//[+] Addition -> number = 1 + 2;
 		int result = number2 + number3;
-		System.out.println(result);					//[-] Subtraction -> number = 1 - 2;
+		System.out.println(result);			//[-] Subtraction -> number = 1 - 2;
 		
-		int number4 = 1;							//[*] Multiplication -> number = 1 * 2;
+		int number4 = 1;				//[*] Multiplication -> number = 1 * 2;
 		int number5 = 2;
 		System.out.println(number4 + number5);		//[/] Division -> number = 1 / 2;
 		
