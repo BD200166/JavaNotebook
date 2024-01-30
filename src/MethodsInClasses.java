5f3076922c727f6d8ae5c52e3a6dd331afdccb57
BD200166 <156713863+BD200166@users.noreply.github.com>
2024-01-30 20:56:29 +0100

    Update MethodsInClasses.java

--- src/MethodsInClasses.java
+++ src/MethodsInClasses.java
@@ -3,13 +3,13 @@ public class MethodsInClasses {
 
 	public static void main(String[] args) {
 	  //Instantiation of an object	
-		//ClassCar car1 = new ClassCar("Grün", "VW", 130);									//Each method is defined within a class
+		//ClassCar car1 = new ClassCar("Grün", "VW", 130);					//Each method is defined within a class
 		//car1.drive();	//"Object/Variable name"."method name"(); -> to call/use method			
-																							//Method name in italics (kursiv) indicates that it is a static method
+													//Method name in italics (kursiv) indicates that it is a static method
 												
-																							//Keyword "static" means the method is static, i.e. the method is not bound to an object of a class
+													//Keyword "static" means the method is static, i.e. the method is not bound to an object of a class
 		
-																							//Methods without the keyword "static" can be used to define individual functions for individual objects
+													//Methods without the keyword "static" can be used to define individual functions for individual objects
 	}
 
 }
