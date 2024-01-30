d546959234bb2cc4690dc629509ef717a4a517e9
BD200166 <156713863+BD200166@users.noreply.github.com>
2024-01-30 20:46:56 +0100

    Update ClassCar.java

--- src/ClassCar.java
+++ src/ClassCar.java
@@ -1,12 +1,12 @@
 
-public class ClassCar {		//ClassCar is a complex DataType	//First letter of a class-file should always be written in capital letters -> better overview to find classes faster
+public class ClassCar {	//ClassCar is a complex DataType	//First letter of a class-file should always be written in capital letters -> better overview to find classes faster
 	
-	//Attributes/properties 									//Classes contain attributes and methods
+	//Attributes/properties 				//Classes contain attributes and methods
 	private String color;																		   
-	private String brand;										//The keywords "public" and "private" are visibility modifier
+	private String brand;					//The keywords "public" and "private" are visibility modifier
 	private int horsePower;																		  
-																//Files | "ClassCar.java" | "PropertiesAndAttributes.java" | "Constructor.java" | "MethodsOverloaded.java" | Legacy.java | ClassDog.java | 
-																//| ClassCat.java | | ClassAnimal.java | are related to file |"Classes.java"| and each other
+								//Files | "ClassCar.java" | "PropertiesAndAttributes.java" | "Constructor.java" | "MethodsOverloaded.java" | Legacy.java | ClassDog.java | 
+								//| ClassCat.java | | ClassAnimal.java | are related to file |"Classes.java"| and each other
 	
 	public ClassCar() {	 //Overloaded constructor -> ClassCar(transfer parameter) [no transfer parameter] -> different signature
 		System.out.println("Auto wurde erzeugt.");
@@ -36,7 +36,7 @@ public class ClassCar {		//ClassCar is a complex DataType	//First letter of a cl
 
 	public String getColor() {		//Setters (setAttributename) and getters (getAttributename)
 		return color;
-	}								//You can use if-clauses in set-methods and get-methods to set precise access conditions
+	}					//You can use if-clauses in set-methods and get-methods to set precise access conditions
 
 	public void setColor(String color) {
 		this.color = color;
